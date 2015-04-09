a587cf86bde3b81f136601954da3f6dd6a68eb88
Elise Fung <elisefung8@gmail.com>
2015-04-09 18:16:01 -0400

    setup views and controllers - routing works!

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,32 @@
-$(document).foundation();
+var app = angular.module('PetfinderApp', ['ngRoute']);
 
-var app = angular.module('PetfinderApp', []);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'views/search.html',
+        controller: 'SearchController'
+    }).
+    when('/pets/:petID', {
+        templateUrl: 'views/pet.html',
+        controller: 'PetController'
+    }).
+    when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileController'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+}]);
 
-app.controller('HomeController', function ($scope) {
-    $scope.hello = "Hello World from Petfinder Controller!";
+app.controller('SearchController', function ($scope, $http) {
+    $scope.hello = "Hello World from Search Controller!";
+});
+
+app.controller('PetController', function ($scope, $http) {
+    $scope.hello = "Hello World from Pet Controller!";
+});
+
+app.controller('ProfileController', function ($scope, $http) {
+    $scope.hello = "Hello World from Profile Controller!";
 });
