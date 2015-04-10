d984e34847e2275b5689d1157d2e032a034bba2e
Elise Fung <elisefung8@gmail.com>
2015-04-10 17:44:02 -0400

    setup mongoose

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// -------------------------------------------------------------------
+// SERVER SETUP
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/petfinder');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -27,3 +32,39 @@ app.get('/', function (request, response) {
 app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+// -------------------------------------------------------------------
+// DATABASE SETUP
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+
+    // Schemas
+    var PetSchema = new mongoose.Schema({
+        name: String,
+        id: String,
+        picture: String
+    });
+    var UserSchema = new mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        id: String,
+        email: String,
+        password: String,
+        favorites: [{
+            id: String
+        }]
+    });
+
+    // Models
+    var Pet = mongoose.model('Pet', PetSchema);
+    var User = mongoose.model('User', UserSchema);
+
+    var cooper = new Pet({
+        name: 'Cooper',
+        id: '1',
+        picture: '1'
+    });
+    console.log(cooper.name);
+});
