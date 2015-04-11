08ade0d52e01b6f5d6e8ec55e66aa4c56cc3cbd8
Elise Fung <elisefung8@gmail.com>
2015-04-10 21:44:22 -0400

    added login and signup forms

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 var app = angular.module('PetfinderApp', ['ngRoute']);
 
+// Configure Routing
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
     when('/', {
@@ -26,3 +27,32 @@ app.config(['$routeProvider', function ($routeProvider) {
         redirectTo: '/'
     });
 }]);
+
+// Pet Search Factory
+app.factory('PetSearch', function PetSearch($http) {
+
+    // array of pet IDs
+    var favorites = [];
+
+    // add the given pet to the list of favorites
+    var addToFavorites = function (pet) {
+        favorites.push(pet);
+    };
+
+    // remove the given pet from the list of favorites
+    var removeFromFavorites = function (pet) {
+        var index = favorites.indexOf(pet);
+        favorites.splice(index, 1);
+    };
+
+    // return list of favorite pets
+    var getFavorites = function () {
+        return favorites;
+    };
+
+});
+
+// API data
+var apikey = "5ac1149d1668e6e3cfb18d3556ef8d79";
+var apisig = "59b0c48e2afad3c8bba73b5659bf4a8d";
+var baseUrl = "http://api.petfinder.com/";
