3b2f28d9c20690876ada24fbe9d201eb80e5ba9b
Elise Fung <elisefung8@gmail.com>
2015-04-18 11:43:50 -0400

    pet page layout

--- public/views/pet/pet.js
+++ public/views/pet/pet.js
@@ -9,9 +9,4 @@ app.controller('PetController', function ($scope, $http, $rootScope, $routeParam
         $scope.$apply();
     });
 
-    //        $scope.addToFavorites = function (pet) {
-    //        // if logged in
-    //            // PetFactory.addToFavorites(pet)
-    //        // else redirect to /login/sign up alert
-    //                    }
 });
